16671db739a2e1b88691317f764d40fb9907e32d
RohitRSS <surwaserohit@gmail.com>
2016-11-28 09:33:11 +0530

    Splash Screen - Right Way demo started

---
+++ SplashScreenRightWay/app/src/main/java/com/rohitss/splashscreenrightway/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.rohitss.splashscreenrightway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
