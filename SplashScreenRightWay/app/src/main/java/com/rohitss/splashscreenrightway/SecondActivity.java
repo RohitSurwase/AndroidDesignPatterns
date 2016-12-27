70e3cab05ce1868b2a5dec0c987c21bc632591a9
RohitRSS <surwaserohit@gmail.com>
2016-12-27 08:31:11 +0530

    splash screen in right way demo app added

---
+++ SplashScreenRightWay/app/src/main/java/com/rohitss/splashscreenrightway/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.rohitss.splashscreenrightway;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by JayYogeshwar on 27-12-2016.
+ */
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
--- SplashScreenRightWay/app/src/main/java/com/rohitss/splashscreenrightway/SplashActivity.java
+++ SplashScreenRightWay/app/src/main/java/com/rohitss/splashscreenrightway/SplashActivity.java
@@ -1,13 +1,29 @@
 package com.rohitss.splashscreenrightway;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+//        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), SecondActivity.class));
+            }
+        }, 2000);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        finish();
     }
 }
