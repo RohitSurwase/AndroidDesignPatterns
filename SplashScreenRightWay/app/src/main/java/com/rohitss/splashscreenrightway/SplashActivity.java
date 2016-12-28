5e374668a08fb62bc206b97e0aa3aaa464c0dfa7
RohitRSS <surwaserohit@gmail.com>
2016-12-28 08:24:17 +0530

    splash screen in right way demo app added

--- SplashScreenRightWay/app/src/main/java/com/rohitss/splashscreenrightway/SplashActivity.java
+++ SplashScreenRightWay/app/src/main/java/com/rohitss/splashscreenrightway/SplashActivity.java
@@ -4,13 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_splash);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
