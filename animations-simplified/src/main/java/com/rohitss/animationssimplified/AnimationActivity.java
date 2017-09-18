9b61a2dfe5de10f367dde36fa526cf7dea4842ca
RohitSurwase <RohitSurwase@users.noreply.github.com>
2017-09-18 19:30:27 +0530

    animations-simplified module in progress

--- animations-simplified/src/main/java/com/rohitss/animationssimplified/AnimationActivity.java
+++ animations-simplified/src/main/java/com/rohitss/animationssimplified/AnimationActivity.java
@@ -1,7 +1,7 @@
 package com.rohitss.animationssimplified;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class AnimationActivity extends AppCompatActivity {
     @Override
