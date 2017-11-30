ebf7d71006b71111b578a6d16a419879769656f0
-global <-global>
2017-11-30 18:59:21 +0530

    updated dependencies

--- dynamic-recycler-view/src/main/java/com/rohitss/dynamicrecycler/DynamicRecyclerActivity.java
+++ dynamic-recycler-view/src/main/java/com/rohitss/dynamicrecycler/DynamicRecyclerActivity.java
@@ -3,10 +3,10 @@ package com.rohitss.dynamicrecycler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class DynamicRecyclerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dynamic_recycler);
     }
 }
